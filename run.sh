07c6193821ab7331a17c9c6ded8fd81e0af65795
davik3000 <davik3000@users.noreply.github.com>
2017-03-06 14:40:23 +0100

    Fixed run.sh script with network creation

--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
+#~/bin/bash
+
+#set -x
+
 ID="docker-tomcat"
+NETWORK="inetwork"
 PORTS="-p 8005:8005 -p 8009:8009 -p 8080:8080 -p 8443:8443"
 IMAGE="davik3000/docker/tomcat"
 
-sudo docker run -d --name ${ID} -h ${ID} --network inetwork ${PORTS} ${IMAGE}
+# detect network
+_network_present=$(sudo docker network ls | grep -e ${NETWORK})
+
+if [ -z "${_network_present}" ] ; then
+  sudo docker network create ${NETWORK}
+fi;
+
+# execute container
+sudo docker run -d --name ${ID} -h ${ID} --network ${NETWORK} ${PORTS} ${IMAGE}
